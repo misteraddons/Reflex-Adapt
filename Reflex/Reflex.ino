5d6d97da28f14dd9fb6a35a9f10912e3f3ed7e46
sonik-br <16923826+sonik-br@users.noreply.github.com>
2023-07-09 20:43:49 -0300

    Better PSX extra modes descriptions

--- Reflex/Reflex.ino
+++ Reflex/Reflex.ino
@@ -225,8 +225,7 @@ uint32_t colors = 0;
         #ifdef SNES_ENABLE_VBOY
           display.setCol(2*6);
           display.print(F("NES + SNES + VBOY"));
-        #endif
-        #ifndef SNES_ENABLE_VBOY
+        #else
           display.setCol(6*6);
           display.print(F("NES + SNES"));
         #endif
@@ -237,13 +236,42 @@ uint32_t colors = 0;
 #ifdef ENABLE_REFLEX_PSX_JOG
       case RZORD_PSX_JOG:
 #endif
-        display.setCol(5*6);
-        display.print(F("PLAYSTATION"));
-#ifdef ENABLE_REFLEX_PSX_JOG
-        if(deviceMode == RZORD_PSX_JOG)
-          display.print(F("-JOGCON"));
-#endif
+      {
+#ifdef ENABLE_REFLEX_PSX_JOG //dedicated jogcon mode
+        if(deviceMode == RZORD_PSX_JOG) {
+          display.setCol(6*6);
+          display.println("PSX JOGCON");
+        } else
+#else //general psx mode
+        {
+          uint8_t psxchars = 9;
+          #ifdef GUNCON_SUPPORT
+            psxchars -= 2;
+          #endif
+          #ifdef JOGCON_SUPPORT
+            psxchars -= 2;
+          #endif
+          #ifdef NEGCON_SUPPORT
+            psxchars -= 2;
+          #endif
+          
+          display.setCol(psxchars*6);
+          display.print(F("PSX"));
+          
+          #ifdef GUNCON_SUPPORT
+            display.print(F("+GUN"));
+          #endif
+          #ifdef JOGCON_SUPPORT
+            display.print(F("+JOG"));
+          #endif
+          #ifdef NEGCON_SUPPORT
+            display.print(F("+NEG"));
+          #endif
+        }
+#endif //end general psx mode
+      
         break;
+      }
 #endif
 #ifdef ENABLE_REFLEX_PCE
       case RZORD_PCE:
